826f9090306c6c484e3424e6bb6e6c7992005da8
hieuhvhc-create <hieuhvhc@gmail.com>
2026-07-13 16:26:13 +0700

    Update data.js

--- data.js
+++ data.js
@@ -47,7 +47,7 @@ const diTich = [
     lat: 15.964949682778094,
     lng: 108.05078200152602,
     xepHang: "Di tích Lịch sử Cách mạng",
-    moTa: "Khu căn cứ cách mạng Huyện ủy Hòa Vang nằm tại khu vực Phú Túc – Ô Rây – Tống Cói, thuộc xã Hòa Phú và Hòa Khương, thành phố Đà Nẵng. Được thành lập năm 1960, đây là căn cứ địa quan trọng của Huyện ủy Hòa Vang và có thời điểm là nơi đứng chân của Thành ủy Đà Nẵng trong kháng chiến chống Mỹ. Từ căn cứ này, nhiều chủ trương, nghị quyết quan trọng được ban hành, góp phần lãnh đạo phong trào cách mạng, xây dựng lực lượng và đấu tranh giải phóng quê hương. Di tích là biểu tượng của tinh thần kiên cường, bất khuất và truyền thống cách mạng của quân và dân Hòa Vang.",
+    moTa: "Khu căn cứ cách mạng Huyện ủy Hòa Vang nằm tại khu vực Phú Túc – Ô Rây – Tống Cói, thuộc xã Hòa Vang và Hòa Tiến, thành phố Đà Nẵng. Được thành lập năm 1960, đây là căn cứ địa quan trọng của Huyện ủy Hòa Vang và có thời điểm là nơi đứng chân của Thành ủy Đà Nẵng trong kháng chiến chống Mỹ. Từ căn cứ này, nhiều chủ trương, nghị quyết quan trọng được ban hành, góp phần lãnh đạo phong trào cách mạng, xây dựng lực lượng và đấu tranh giải phóng quê hương. Di tích là biểu tượng của tinh thần kiên cường, bất khuất và truyền thống cách mạng của quân và dân Hòa Vang.",
     hinhAnh: "images/cancu.jpg",
     googleMaps: "https://www.google.com/maps?q=15.964949682778094,108.05078200152602",
     video: "https://www.youtube.com/embed/zzdjX5SoZEg"
